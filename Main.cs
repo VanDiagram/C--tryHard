bc40669827c085e8d58ebe0c45955e983036334b
Tokkiory <teamchampionsvietnam.com>
2023-10-19 23:23:40 +0700

    update

---
+++ Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hellish
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("This is the begin of another hellish project: ");
+            string quote = Console.ReadLine();
+
+            Console.WriteLine("Alrighty: {0}", quote);
+        }
+    }
+}
